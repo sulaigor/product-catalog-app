35e3f26cd453cdcf4d08266e40f022162e82c591
Igor Šula <sulaigor@gmail.com>
2021-08-22 00:33:54 +0200

    Add load products button to catalog screen component

--- src/screens/CatalogScreen/CatalogScreen.tsx
+++ src/screens/CatalogScreen/CatalogScreen.tsx
@@ -1,7 +1,13 @@
+import LoadProductsButton from 'components/LoadProductsButton';
 import ProductsList from 'components/ProductsList';
 
 const CatalogScreen = () => {
-  return <ProductsList />;
+  return (
+    <>
+      <ProductsList />
+      <LoadProductsButton />
+    </>
+  );
 };
 
 export default CatalogScreen;
