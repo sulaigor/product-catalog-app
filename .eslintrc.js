1fe30869c0ef02f80328495f990a3687a2e2ff79
Igor Šula <sulaigor@gmail.com>
2021-08-21 19:19:54 +0200

    Edit eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -48,6 +48,7 @@ module.exports = {
     'jsx-a11y/click-events-have-key-events': 'off',
     'react-hooks/exhaustive-deps': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-shadow': 'error',
