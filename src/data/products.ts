09d75d0dfa0fc5f942ccdf07c88927d2f3eda944
Igor Šula <sulaigor@gmail.com>
2021-08-21 19:22:48 +0200

    Add products data singleton

---
+++ src/data/products.ts
@@ -0,0 +1,29 @@
+import { IProduct, Product } from 'models/Product';
+import { getRandomInteger } from 'utils/random';
+
+// Products data singleton
+let products: IProduct[] | null = null;
+
+const generateProducts = () => {
+  const productsCount = getRandomInteger(100, 90);
+  products = [];
+
+  while (products.length < productsCount) {
+    products.push(new Product());
+  }
+};
+
+export const getProductsData = (): Promise<IProduct[]> => {
+  if (!products) {
+    // Generate products once
+    generateProducts();
+  }
+
+  // Simulate API call
+  return new Promise((resolve, reject) =>
+    setTimeout(() => {
+      if (products) resolve(products);
+      else reject(new Error('Products not exists!'));
+    }, 500)
+  );
+};
