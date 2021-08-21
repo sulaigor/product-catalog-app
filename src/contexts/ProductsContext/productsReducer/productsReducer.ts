daa8912adc57fe91de382ba34d5eb0165a83a863
Igor Šula <sulaigor@gmail.com>
2021-08-21 21:50:56 +0200

    Add new cases to products reducer

--- src/contexts/ProductsContext/productsReducer/productsReducer.ts
+++ src/contexts/ProductsContext/productsReducer/productsReducer.ts
@@ -1,10 +1,24 @@
+import { IProduct } from 'models/Product';
 import { ReducerFunctionType } from 'types/reducer';
+import { LOAD_PRODUCTS, SET_PRODUCTS } from './actionTypes';
 import { IReducerState } from './types';
 
 const productsReducer: ReducerFunctionType<IReducerState> = (state, action) => {
-  const { type } = action;
+  const { type, payload } = action;
 
   switch (type) {
+    case SET_PRODUCTS:
+      return {
+        ...state,
+        products: payload?.newProducts,
+      };
+
+    case LOAD_PRODUCTS:
+      return {
+        ...state,
+        products: [...(state.products as IProduct[]), ...payload?.newProducts],
+      };
+
     default:
       return state;
   }
