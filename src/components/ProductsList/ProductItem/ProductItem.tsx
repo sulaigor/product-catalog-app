60a8e2dd0fd3595561ad73c1c507bff33dcc188f
Igor Šula <sulaigor@gmail.com>
2021-08-22 00:34:18 +0200

    Edit look of product item component

--- src/components/ProductsList/ProductItem/ProductItem.tsx
+++ src/components/ProductsList/ProductItem/ProductItem.tsx
@@ -1,13 +1,20 @@
 import { IProduct } from 'models/Product';
+import Title from 'components/Title';
+import Paragraph from 'components/Paragraph';
 
 interface IProps {
   product: IProduct;
 }
 
 const ProductItem = ({ product }: IProps) => {
-  const { name } = product;
+  const { name, description } = product;
 
-  return <div>{name}</div>;
+  return (
+    <div>
+      <Title>{name}</Title>
+      <Paragraph>{description}</Paragraph>
+    </div>
+  );
 };
 
 export default ProductItem;
