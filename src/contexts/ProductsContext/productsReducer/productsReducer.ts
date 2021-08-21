f1310f80df8856b5c270b87c33d096d9d3a92285
Igor Šula <sulaigor@gmail.com>
2021-08-21 23:22:41 +0200

    Add loading cases to products reducer

--- src/contexts/ProductsContext/productsReducer/productsReducer.ts
+++ src/contexts/ProductsContext/productsReducer/productsReducer.ts
@@ -1,6 +1,6 @@
 import { IProduct } from 'models/Product';
 import { ReducerFunctionType } from 'types/reducer';
-import { LOAD_PRODUCTS, SET_PRODUCTS } from './actionTypes';
+import { LOAD_PRODUCTS, SET_LOADING, SET_PRODUCTS, UNSET_LOADING } from './actionTypes';
 import { IReducerState } from './types';
 
 const productsReducer: ReducerFunctionType<IReducerState> = (state, action) => {
@@ -17,6 +17,18 @@ const productsReducer: ReducerFunctionType<IReducerState> = (state, action) => {
       return {
         ...state,
         products: [...(state.products as IProduct[]), ...payload?.newProducts],
+      };
+
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+
+    case UNSET_LOADING:
+      return {
+        ...state,
+        loading: false,
       };
 
     default:
