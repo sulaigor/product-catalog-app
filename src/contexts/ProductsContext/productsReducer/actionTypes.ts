dc65619f2787d993f24a04f88f2e3b39164c19a6
Igor Šula <sulaigor@gmail.com>
2021-08-21 23:21:44 +0200

    Add new actions to products reducer

--- src/contexts/ProductsContext/productsReducer/actionTypes.ts
+++ src/contexts/ProductsContext/productsReducer/actionTypes.ts
@@ -1,2 +1,5 @@
 export const SET_PRODUCTS = 'SET_PRODUCTS';
 export const LOAD_PRODUCTS = 'LOAD_PRODUCTS';
+
+export const SET_LOADING = 'SET_LOADING';
+export const UNSET_LOADING = 'UNSET_LOADING';
--- src/contexts/ProductsContext/productsReducer/actions.ts
+++ src/contexts/ProductsContext/productsReducer/actions.ts
@@ -1,6 +1,6 @@
 import { IProduct } from 'models/Product';
 import { IReducerAction } from 'types/reducer';
-import { LOAD_PRODUCTS, SET_PRODUCTS } from './actionTypes';
+import { LOAD_PRODUCTS, SET_LOADING, SET_PRODUCTS, UNSET_LOADING } from './actionTypes';
 
 export const setProductsAction = (newProducts: IProduct[]): IReducerAction => ({
   type: SET_PRODUCTS,
@@ -10,4 +10,12 @@ export const setProductsAction = (newProducts: IProduct[]): IReducerAction => ({
 export const loadProductsAction = (newProducts: IProduct[]): IReducerAction => ({
   type: LOAD_PRODUCTS,
   payload: { newProducts },
+});
+
+export const setLoadingAction = (): IReducerAction => ({
+  type: SET_LOADING,
+});
+
+export const unsetLoadingAction = (): IReducerAction => ({
+  type: UNSET_LOADING,
 });
