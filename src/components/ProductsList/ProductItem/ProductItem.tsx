669a767a0d71a11927316891f2b78f14e8d6a0fd
Igor Šula <sulaigor@gmail.com>
2021-08-22 23:31:06 +0200

    Add usage of parameters component in product item

--- src/components/ProductsList/ProductItem/ProductItem.tsx
+++ src/components/ProductsList/ProductItem/ProductItem.tsx
@@ -3,6 +3,7 @@ import Title from 'components/Title';
 import Paragraph from 'components/Paragraph';
 import Image from 'components/Image';
 import Ratings from 'components/Ratings';
+import Parameters from 'components/Parameters';
 import css from './productItem.module.scss';
 
 interface IProps {
@@ -10,12 +11,15 @@ interface IProps {
 }
 
 const ProductItem = ({ product }: IProps) => {
-  const { name, description, image, ratings } = product;
+  const { name, description, image, ratings, parameters } = product;
 
   return (
     <section className={css.productItem}>
       <div className={css.imageWrapper}>
         <Image url={image} alt={name} />
+        <div className={css.paramsWrapper}>
+          <Parameters className={css.params} parameters={parameters} />
+        </div>
       </div>
       <Title>{name}</Title>
       <Paragraph className={css.text}>{description}</Paragraph>
