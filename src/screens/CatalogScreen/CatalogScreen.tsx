441c837a7d1fcf867e158cf179d98ba8ab59ec86
Igor Šula <sulaigor@gmail.com>
2021-08-22 21:30:12 +0200

    Add usage of screen component in catalog screen

--- src/screens/CatalogScreen/CatalogScreen.tsx
+++ src/screens/CatalogScreen/CatalogScreen.tsx
@@ -1,14 +1,15 @@
+import Screen from 'components/Screen';
 import LoadProductsButton from 'components/LoadProductsButton';
 import ProductsList from 'components/ProductsList';
 import CatalogTitle from './CatalogTitle';
 
 const CatalogScreen = () => {
   return (
-    <>
+    <Screen>
       <CatalogTitle />
       <ProductsList />
       <LoadProductsButton />
-    </>
+    </Screen>
   );
 };
 
