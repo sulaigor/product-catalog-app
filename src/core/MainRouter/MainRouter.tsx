fe3f77536d1d0fccaffa30a02d1db4ca3638fd1c
Igor Šula <sulaigor@gmail.com>
2021-08-22 23:57:11 +0200

    Add product detail screen component to main router

--- src/core/MainRouter/MainRouter.tsx
+++ src/core/MainRouter/MainRouter.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { DEFAULT_PATH, NOT_FOUND_PATH } from 'const/screenPaths';
+import { DEFAULT_PATH, NOT_FOUND_PATH, PRODUCT_DETAIL_PATH } from 'const/screenPaths';
 import CatalogScreen from 'screens/CatalogScreen';
 import NotFoundScreen from 'screens/NotFoundScreen';
+import ProductDetailScreen from 'screens/ProductDetailScreen';
 import NotFoundRedirect from 'components/NotFoundRedirect';
 
 const MainRouter = () => {
@@ -10,6 +11,9 @@ const MainRouter = () => {
       <Switch>
         <Route exact path={DEFAULT_PATH}>
           <CatalogScreen />
+        </Route>
+        <Route exact path={PRODUCT_DETAIL_PATH}>
+          <ProductDetailScreen />
         </Route>
         <Route path={NOT_FOUND_PATH}>
           <NotFoundScreen />
