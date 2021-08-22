5c98ea4fc46017c02a466e421ad093b82041e375
Igor Šula <sulaigor@gmail.com>
2021-08-22 22:45:04 +0200

    Add not found screen to main router core component

--- src/core/MainRouter/MainRouter.tsx
+++ src/core/MainRouter/MainRouter.tsx
@@ -1,13 +1,21 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { DEFAULT_PATH } from 'const/screenPaths';
+import { DEFAULT_PATH, NOT_FOUND_PATH } from 'const/screenPaths';
 import CatalogScreen from 'screens/CatalogScreen';
+import NotFoundScreen from 'screens/NotFoundScreen';
+import NotFoundRedirect from 'components/NotFoundRedirect';
 
 const MainRouter = () => {
   return (
     <Router>
       <Switch>
-        <Route path={DEFAULT_PATH}>
+        <Route exact path={DEFAULT_PATH}>
           <CatalogScreen />
+        </Route>
+        <Route path={NOT_FOUND_PATH}>
+          <NotFoundScreen />
+        </Route>
+        <Route path='*'>
+          <NotFoundRedirect />
         </Route>
       </Switch>
     </Router>
