c796b345fb6331429ff956a37120cc3b6433725f
Igor Šula <sulaigor@gmail.com>
2021-08-22 23:59:41 +0200

    Add link to product detail screen in product item comp

--- src/components/ProductsList/ProductItem/ProductItem.tsx
+++ src/components/ProductsList/ProductItem/ProductItem.tsx
@@ -1,9 +1,11 @@
 import { IProduct } from 'models/Product';
+import { getProductDetailPath } from 'utils/router';
 import Title from 'components/Title';
 import Paragraph from 'components/Paragraph';
 import Image from 'components/Image';
 import Ratings from 'components/Ratings';
 import Parameters from 'components/Parameters';
+import Link from 'components/Link';
 import css from './productItem.module.scss';
 
 interface IProps {
@@ -11,22 +13,24 @@ interface IProps {
 }
 
 const ProductItem = ({ product }: IProps) => {
-  const { name, description, image, ratings, parameters } = product;
+  const { id: productId, name, description, image, ratings, parameters } = product;
 
   return (
-    <section className={css.productItem}>
-      <div className={css.imageWrapper}>
-        <Image url={image} alt={name} />
-        <div className={css.paramsWrapper}>
-          <Parameters className={css.params} parameters={parameters} />
+    <Link to={getProductDetailPath(productId)}>
+      <section className={css.productItem}>
+        <div className={css.imageWrapper}>
+          <Image url={image} alt={name} />
+          <div className={css.paramsWrapper}>
+            <Parameters className={css.params} parameters={parameters} />
+          </div>
         </div>
-      </div>
-      <Title>{name}</Title>
-      <Paragraph className={css.text}>{description}</Paragraph>
-      <div className={css.ratingsWrapper}>
-        <Ratings ratings={ratings} />
-      </div>
-    </section>
+        <Title>{name}</Title>
+        <Paragraph className={css.text}>{description}</Paragraph>
+        <div className={css.ratingsWrapper}>
+          <Ratings ratings={ratings} />
+        </div>
+      </section>
+    </Link>
   );
 };
 
