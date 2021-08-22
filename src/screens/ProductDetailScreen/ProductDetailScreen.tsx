8e5e1e6e625c9b727b7fe7cf4e95d2513e806974
Igor Šula <sulaigor@gmail.com>
2021-08-23 00:35:05 +0200

    Add back link to product detail screen

--- src/screens/ProductDetailScreen/ProductDetailScreen.tsx
+++ src/screens/ProductDetailScreen/ProductDetailScreen.tsx
@@ -1,10 +1,13 @@
 import { useMemo } from 'react';
 import { useParams } from 'react-router-dom';
+import { DEFAULT_PATH } from 'const/screenPaths';
 import { useProductsContext } from 'contexts/ProductsContext';
 import Screen from 'components/Screen';
+import Link from 'components/Link';
 import NotFoundRedirect from 'components/NotFoundRedirect';
 import ProductDetail from './ProductDetail';
 import { IProductDetailParams } from './types';
+import css from './productDetailScreen.module.scss';
 
 const ProductDetailScreen = () => {
   const { productId } = useParams<IProductDetailParams>();
@@ -23,6 +26,9 @@ const ProductDetailScreen = () => {
 
   return (
     <Screen>
+      <div className={css.linkWrapper}>
+        <Link to={DEFAULT_PATH}>↩ Go back</Link>
+      </div>
       <ProductDetail product={product} />
     </Screen>
   );
