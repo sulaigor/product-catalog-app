daf32103486f68a09e0d84e35b8b537cd9a3c19e
Igor Šula <sulaigor@gmail.com>
2021-08-22 23:55:42 +0200

    Add product detail path and params to screen paths

--- src/const/screenPaths.ts
+++ src/const/screenPaths.ts
@@ -1,2 +1,5 @@
+export const PRODUCT_DETAIL_PARAMS = ':productId';
+
 export const DEFAULT_PATH = '/';
+export const PRODUCT_DETAIL_PATH = `/product/${PRODUCT_DETAIL_PARAMS}`;
 export const NOT_FOUND_PATH = '/404';
