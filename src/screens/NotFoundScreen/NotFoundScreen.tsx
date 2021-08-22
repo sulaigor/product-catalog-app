95e9b1900db8d3136bed91dad5e1db5d6d53cc04
Igor Šula <sulaigor@gmail.com>
2021-08-22 23:03:02 +0200

    Add home link to not found screen

--- src/screens/NotFoundScreen/NotFoundScreen.tsx
+++ src/screens/NotFoundScreen/NotFoundScreen.tsx
@@ -2,6 +2,10 @@ import Screen from 'components/Screen';
 import Empty from 'components/Empty';
 import Paragraph from 'components/Paragraph';
 import Title from 'components/Title';
+import Center from 'components/Center';
+import Link from 'components/Link';
+import { DEFAULT_PATH } from 'const/screenPaths';
+import css from './notFoundScreen.module.scss';
 
 const NotFoundScreen = () => {
   return (
@@ -10,6 +14,9 @@ const NotFoundScreen = () => {
         <Title>404</Title>
         <Paragraph>Page not found...</Paragraph>
       </Empty>
+      <Center className={css.linkWrapper}>
+        <Link to={DEFAULT_PATH}>Go home</Link>
+      </Center>
     </Screen>
   );
 };
